af18c7f32e39fc519d57d7d1860e06dea27c8368
ER1707 <ewan.17.remo@gmail.com>
2025-03-24 09:47:40 +0100

    page login

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+)
+
+// Handler pour la page d'accueil
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl, err := template.ParseFiles("templates/index.html")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	tmpl.Execute(w, nil)
+}
+
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl, err := template.ParseFiles("templates/login.html")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	tmpl.Execute(w, nil)
+}
+
+func main() {
+	// Définir le handler pour la route /
+
+	http.Handle("/styles/", http.StripPrefix("/styles/", http.FileServer(http.Dir("styles"))))
+	http.HandleFunc("/login", loginHandler)
+
+	http.HandleFunc("/", homeHandler)
+
+	// Définir le port du serveur
+	port := ":8080"
+	fmt.Println("Serveur démarré sur http://localhost" + port)
+
+	// Démarrer le serveur
+	http.ListenAndServe(port, nil)
+}
